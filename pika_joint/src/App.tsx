42b2105dc332d0ce91b81dddb65322b17238249c
alberto1996-23 <albertowilliams16@gmail.com>
2026-04-23 17:42:05 -0500

    Add React frontend setup for The Pika Joint

---
+++ pika_joint/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>The Pika Joint</h1>
+      <p>Welcome to the Pokémon-inspired restaurant ordering system.</p>
+      <button>View Menu</button>
+    </main>
+  )
+}
+
+export default App
